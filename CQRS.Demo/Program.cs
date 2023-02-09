11b75e6431bffae7d9e00a21a834d13b4f1e5218
Dev-khatab <khatap1@hotmail.com>
2023-02-09 12:59:39 -0800

    Add, using MediatR

--- CQRS.Demo/Program.cs
+++ CQRS.Demo/Program.cs
@@ -1,6 +1,12 @@
+using MediatR;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+//  MediatR Method
+
+builder.Services.AddMediatR(typeof(Program));
 
 builder.Services.AddControllers();
     
